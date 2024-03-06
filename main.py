29891fc20db627b38cdd89cec7d7dd2afbe71892
Kush <kokatekush@gmail.com>
2024-03-07 00:25:42 +0530

    initial commit

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-reps = 1 * 60
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
-
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text = "00:00")
+    title_label.config(text = "Timer")
+    check_marks.config(text = "")
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
@@ -44,9 +51,16 @@ def count_down(count):
         count_second = "0" + str(count_second)
     canvas.itemconfig(timer_text, text = f"{count_minute}:{count_second}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(count / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -57,7 +71,7 @@ title_label = Label(text="Timer",fg=GREEN, font=(FONT_NAME,30,"bold"), bg=YELLOW
 title_label.grid(row=0, column=1)
 button = Button(text="Start", command=start_timer)
 button.grid(row = 2, column =0)
-button2 = Button(text="Reset")
+button2 = Button(text="Reset", command=reset_timer)
 button2.grid(row =2, column=2)
 
 
@@ -68,7 +82,7 @@ canvas.create_image(100, 100, image=tomato_img)
 timer_text = canvas.create_text(100, 125, text="00:00", fill= "white", font=(FONT_NAME, 20, "bold"))
 canvas.grid(column=1, row=1)
 
-check_marks = Label(text ="✔", fg=GREEN, bg=YELLOW)
+check_marks = Label( fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 
